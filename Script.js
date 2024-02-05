91d61f5cf33ed4caec5cfeb6c3204a9969439b05
Adam Huss <adamhuss13@gmail.com>
2024-02-05 15:01:14 -0600

    Added questions outline

---
+++ Script.js
@@ -0,0 +1,16 @@
+//Questions
+
+//Commonly used data types do not include____.
+//A. strings B. booleans C. alerts D. numbers
+
+//The condition in an if/else statement is enclosed with ____.
+//A. quotes B. curly brackets C. parenthesis D. square brackets
+
+//Arrays in JavaScript can be used to store ____.
+//A. strings and numbers, B. other arrays, C. booleans, D. all of the above
+
+//String values must be enclosed within ____ when being assigned to variables.
+//A. commas, B. curly brackets, C. quotes, D. parenthesis
+
+//A very usefull tool used during development and debugging for printing content to the debugger is _____.
+//A. JavaScript, B. terminal/bash, C. for loops, D. console.log
