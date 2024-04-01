44fa5db66bcc0aa8a7eef6a23be59cd9ae549075
Adam Huss <adamhuss13@gmail.com>
2024-04-01 14:33:37 -0500

    changed question format with answers

--- Script.js
+++ Script.js
@@ -1,16 +1,27 @@
-//Questions
-
-//Commonly used data types do not include:
-//A. strings B. booleans C. alerts D. numbers
-
-//The condition in an if/else statement is enclosed with ____.
-//A. quotes B. curly brackets C. parenthesis D. square brackets
-
-//Arrays in JavaScript can be used to store ____.
-//A. strings and numbers, B. other arrays, C. booleans, D. all of the above
-
-//String values must be enclosed within ____ when being assigned to variables.
-//A. commas, B. curly brackets, C. quotes, D. parenthesis
-
-//A very usefull tool used during development and debugging for printing content to the debugger is:
-//A. JavaScript, B. terminal/bash, C. for loops, D. console.log
+let questions = [
+{
+  title: "Commonly used data types DO NOT include:",
+  choices: ["strings", "booleans", "alerts", "numbers"],
+  answer: "alerts"
+},
+{
+  title: "The condition in an if / else statement is enclosed within ____.",
+  choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+  answer: "parentheses"
+},
+{
+  title: "Arrays in Javascript can be used to store ____.",
+  choices: ["numbers and strings", "other arrays", "booleans", "all of the above"],
+  answer: "all of the above"
+},
+{
+  title: "String values must be enclosed within ____ when being assigned to variables.",
+  choices: ["commas", "curly brackets", "quotes", "parenthesis"],
+  answer: "quotes"
+},
+{
+  title: "A very useful tool for used during development and debugging for printing content to the debugger is:",
+  choices: ["Javascript", "terminal / bash", "for loops", "console log"],
+  answer: "console log"
+}
+];
